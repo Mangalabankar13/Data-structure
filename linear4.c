55bacdf81b464f14c95635a44f2aaa1b7dda0d6b
Mangalabankar13 <138298410+Mangalabankar13@users.noreply.github.com>
2023-07-04 14:45:55 +0530

    Add files via upload

---
+++ linear4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+ int a[50],i,n,max1,max2,sum=0;
+ printf("enter limit");
+ scanf("%d",&n);
+ printf("enter array element");
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+ max1=a[0];
+ for(i=0;i<n;i++)
+ {
+  if(max1<a[i])
+  max1=a[i];
+ }
+ printf("\nenter first max no=%d",max1);
+ max2=a[0];
+ for(i=0;i<n;i++)
+ {
+  if(max2<a[i])
+  {
+   if(a[i]!=max1)
+   max2=a[i];
+  }
+ } 
+ printf("\nenter second max no=%d",max2);
+ sum=max1+max2;
+ printf("\nsum=%d",sum);
+}  
