ef259f949bb2871c32afe495fb60748d152977dc
Mangalabankar13 <138298410+Mangalabankar13@users.noreply.github.com>
2023-07-04 13:48:19 +0530

    Add files via upload

---
+++ linear searching3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+ int a[20],b[20],i,j,n1,n2,flag,c[20],k=0;
+ printf("enter first array limit");
+ scanf("%d",&n1);
+  printf("enter first array element");
+  for(i=0;i<n1;i++)
+ scanf("%d",&a[i]);
+  printf("enter second array limit");
+ scanf("%d",&n2);
+ printf("enter second array element");
+ for(j=0;j<n2;j++)
+ scanf("%d",&b[j]);
+ for(i=0;i<n1;i++)
+ c[k++]=a[i];
+ for(i=0;i<n2;i++)
+ {
+ flag=0;
+  for(j=0;j<n1;j++)
+  {
+   if(b[i]==a[j])
+   {
+    flag=1;
+    break;
+   }
+  }
+ if(flag==0)
+   {
+    c[k++]=b[i];
+   }
+ }
+ printf("\nunion of two arrays");
+ for(i=0;i<k;i++)
+ printf("%d ",c[i]);
+}     
