8c69342d2916e405dc3e02a7aff7037a38b4c29d
Mangalabankar13 <138298410+Mangalabankar13@users.noreply.github.com>
2023-07-08 23:02:41 +0530

    Add files via upload

---
+++ bubble sort1.c
@@ -0,0 +1,29 @@
+//bubble sort-accept n student names from user and sort alphabetical order
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+ int pass,i,n;
+ char s1[20][20],t[20];
+ printf("enter limit");
+ scanf("%d",&n);
+ printf("\nenter student name");
+ for(i=0;i<n;i++)
+ scanf("%s",&s1[i]);
+ for(pass=1;pass<n;pass++)
+ {
+  for(i=0;i<n-pass;i++)
+  {
+ if(strcmp(s1[i],s1[i+1])>0)
+ {
+  strcpy(t,s1[i]);
+  strcpy(s1[i],s1[i+1]);
+  strcpy(s1[i+1],t);
+ }
+ }
+} 
+ printf("sorted student name \n");
+ for(i=0;i<n;i++)
+ printf("\n%s",s1[i]);
+}  
